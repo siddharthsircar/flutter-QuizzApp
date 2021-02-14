ff6e583cfbb97ebda877ccd5c1b06b80e6ff6b1c
Siddharth Sircar <sidsircar16@gmail.com>
2021-02-14 20:19:03 +0530

    Added alignment to result text

--- lib/result.dart
+++ lib/result.dart
@@ -25,6 +25,7 @@ class Result extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Text(
           resultPhrase,
